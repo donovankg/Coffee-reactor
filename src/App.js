5699eec8cd31a054ebf07dd0469e4c310782e185
anishrijal <rijalanish@hotmail.com>
2017-01-10 15:48:39 -0600

    sorry i added in newsAPp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import News from './news/news';
+import Weather from  './weatherApp/weatherApp';
 
 class App extends Component {
   render() {
@@ -12,7 +13,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <News />
-
+        <Weather />
       </div>
     );
   }
