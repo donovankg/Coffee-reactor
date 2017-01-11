d07acc3462c67c763d2f164aaa03115670c23133
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2017-01-11 14:38:17 -0600

    dev fix

--- src/App.js
+++ src/App.js
@@ -21,17 +21,7 @@ class App extends Component {
         <Navi></Navi>
         <DashContent>
         </DashContent>
-        <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Coffee-reactor</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Cal />
-        <TwitterFeed />
-        <News />
-        <Weather />
         <Transaction />
       </div>
     );
