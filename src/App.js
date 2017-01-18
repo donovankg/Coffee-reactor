a482132dd50f5541a2237fa2dba4603df3f630f6
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2017-01-18 14:51:49 -0600

    temp

--- src/App.js
+++ src/App.js
@@ -19,55 +19,35 @@ class App extends Component {
   render() {
 
     return (
-
       <div className="App">
-
         <Navi></Navi>
-
         <DashContent>
-
         </DashContent>
-
       </div>
-
     );
-
   }
-
 }
 
 class Navi extends Component {
-
   render() {
-
     return (
-
       <div>
-
-    <nav className="navbar  navbar-fixed-top">
-    <div className="container">
-
-
-  <img src ={logo} className ="logo"/>
-
-    <div className ="navbar-text">
-      <h2>
-        <Clock/>
-      </h2>
+        <nav className="navbar  navbar-fixed-top">
+          <div className="container">
+            <img src ={logo} className ="logo"/>
+            <div className ="navbar-text">
+              <h2>
+                <Clock/>
+              </h2>
+            </div>
+          </div>
+        </nav>
     </div>
-    </div>
-  </nav>
-    </div>
-
-
     );
-
   }
-
 }
 
 const DashContent = React.createClass ({
-
  getInitialState () {
     return { showModal: false};
   },
@@ -78,125 +58,79 @@ const DashContent = React.createClass ({
     this.setState({ showModal: true});
   },
 
-
-
  render () {
-
    return (
-
-     <div className ="container-fluid dash" >
-
-     <div className="row">
-
-   <div className="col-md-4">
-
-     <div className="row">
-
-       <div className="col-md-12">
-
-       <div className ="container-fluid titles">
-
-         <h4>Twitter</h4>
-
-       </div>
-
-         <div id= 'twitter'className="content-top">
-
-           <TwitterFeed />
-
-         </div>
-
-       </div>
-
-       <div className="col-md-12">
-
-        </div>
-          <div  className="content-bottom">
-
+<div className ="container-fluid dash" >
+  <div className="row">
+    <div className="col-md-4">
+      <div className="row">
+        <div className="col-md-12">
+          <div className ="container-fluid titles">
+            <h4>Twitter</h4>
+          </div>
+          <div id= 'twitter'className="content-top">
+            <TwitterFeed />
           </div>
         </div>
         <div className="col-md-12">
+        </div>
+        <div className="content-bottom">
+        </div>
+      </div>
+      <div className="col-md-12">
         <div className ="container-fluid titles">
           <h4>Transaction</h4>
-
-
-         <h4>Music</h4>
-
-       </div>
-
-         <div  className="content-bottom">
-            <iframe id="sc-widget" src="https://w.soundcloud.com/player/?url=http://api.soundcloud.com/users/1539950/favorites" width="100%" height="465" scrolling="no" frameborder="no"></iframe>
-          </div>
-
+          <h4>Music</h4>
         </div>
-
+        <div className="content-bottom">
+          <iframe id="sc-widget" src="https://w.soundcloud.com/player/?url=http://api.soundcloud.com/users/1539950/favorites" width="100%" height="465" scrolling="no" frameborder="no"></iframe>
         </div>
-
-
       </div>
-
-
-     </div>
-
-   </div>
-
-   <div className="col-md-8">
-      <div className="row">
-
-       <div id="news" className="col-md-12">
-          <div className ="container-fluid titles">
-            <h4>News</h4>
-          </div>
-
-         <div  className="content-top" >
-            <News />
-          </div>
-
-       </div>
-
-     </div>
-
-     <div className="row">
-
-       <div className="col-md-9">
-         <div className ="container-fluid titles">
-         <label>
-           <span><h4>Calendar</h4></span>
-           </label>
-           <button type="submit" className="btn btn-default pull-right btn-sm cal-button"
-             onClick = {this.open}>
-
-          Add Event
-           </button>
-           <Modal bsSize="large" show ={this.state.showModal} onHide = {this.close}>
-             <Modal.Body>
-               <h4>Modal is working</h4>
-             </Modal.Body>
-             <Modal.Footer>
-               <Button onClick={this.close}>Close</Button>
-             </Modal.Footer>
-           </Modal>
-
-        </div>
-        <div className="col-md-12 content-top ">
-
-        </div>
-
-      </div>
-
-      <div className="col-md-3">
-
-
-     <div className="col-md-3">
+    </div>
+  </div>
+</div>
+<div className="col-md-8">
+  <div className="row">
+    <div id="news" className="col-md-12">
       <div className ="container-fluid titles">
-        <h4>Weather</h4>
+        <h4>News</h4>
       </div>
-        <div id='weather' className="col-md-12 content-bottom">
-        <Weather />
-        </div>
-
-   </div>
-
+      <div className="content-top" >
+        <News />
+      </div>
+    </div>
+</div>
+<div className="row">
+  <div className="col-md-9">
+    <div className ="container-fluid titles">
+      <label>
+        <span><h4>Calendar</h4></span>
+      </label>
+      <button type="submit" className="btn btn-default pull-right btn-sm cal-button"
+        onClick = {this.open}>
+        Add Event
+      </button>
+      <Modal bsSize="large" show ={this.state.showModal} onHide = {this.close}>
+        <Modal.Body>
+          <h4>Modal is working</h4>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.close}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+    <div className="col-md-12 content-top ">
+    </div>
+  </div>
+<div className="col-md-3">
+  <div className="col-md-3">
+    <div className ="container-fluid titles">
+      <h4>Weather</h4>
+    </div>
+    <div id='weather' className="col-md-12 content-bottom">
+      <Weather />
+    </div>
+  </div>
 </div>
 </div>
 </div>
