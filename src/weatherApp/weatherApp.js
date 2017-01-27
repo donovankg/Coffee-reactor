529e36571f5101cf825e50c1df6474eb3188d528
Kendrick Carroll <kendrickcarrolldc@gmail.com>
2017-01-27 10:28:34 -0600

    updated naming and https calls

--- src/weatherApp/weatherApp.js
+++ src/weatherApp/weatherApp.js
@@ -24,7 +24,7 @@ class Weather extends Component {
   }
 
   WeatherList(newCity){
-    return fetch("http://api.openweathermap.org/data/2.5/forecast?units=metric&q="+newCity+"&mode=json&appid=5babe75ca0e2081709ac0eda2202d4f9")
+    return fetch("https://api.openweathermap.org/data/2.5/forecast?units=metric&q="+newCity+"&mode=json&appid=5babe75ca0e2081709ac0eda2202d4f9")
       .then((response)=>response.json())
       .then((responseJson) =>{
         this.setState({city: responseJson.city.name,
@@ -41,7 +41,7 @@ class Weather extends Component {
            placeholder="The weather in ..." onKeyPress={ this.notify}/>
         <div id="layout-content" className="layout-content-wrapper">
             <div className="main-info-city-name">{ this.state.city }</div>
-            <img src={ "http://openweathermap.org/img/w/" + this.state.icon + ".png" } alt="weather_icon" className="day-info-img" />
+            <img src={ "https://openweathermap.org/img/w/" + this.state.icon + ".png" } alt="weather_icon" className="day-info-img" />
             <div className="main-info-description">{ this.state.description }</div>
 		        <div className="main-info-temp">{ Math.round(((this.state.temp)*1.8)+32) } °F</div>
         </div>
